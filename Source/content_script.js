78fe4c25d7c0971ffea8622e60847bc0ed318075
Frank Denis <github@pureftpd.org>
2015-12-04 15:48:21 +0100

    Initial import

---
+++ Source/content_script.js
@@ -0,0 +1,42 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bThe DGA\b/g, "My Butt");
+	v = v.replace(/\bThe dga\b/g, "My butt");
+	v = v.replace(/\bthe Dga\b/g, "my Butt");
+	v = v.replace(/\bthe dga\b/g, "my butt");
+	
+	textNode.nodeValue = v;
+}
+
+
